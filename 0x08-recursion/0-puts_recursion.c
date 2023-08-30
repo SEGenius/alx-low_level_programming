21e4ca67927e8f05baea2ac6fd4c6eb373a30c84
SEGenius <olutundestephen96@gmail.com>
2023-08-30 07:46:56 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Write a function that prints a string, followed by a new line
+ * _puts_recursion - Function that prints a string, followed by a new line
  * @s: string
  *
  * Return: On success 1.
