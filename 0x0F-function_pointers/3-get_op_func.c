36bd954514411e89da43dc69196d34058b31c077
SEGenius <olutundestephen96@gmail.com>
2023-09-13 19:20:25 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,30 +3,34 @@
 #include <string.h>
 
 /**
- *   * get_op_func - ...
- *     * @s: ...
- *       *
- *         * Return: ...
- *           */
+ * get_op_func - ...
+ * @s: ...
+ *
+ * Return: ...
+*/
 int (*get_op_func(char *s))(int, int)
 {
-		op_t ops[] = {
-				{ "+", op_add },
-					{ "-", op_sub },
-						{ "*", op_mul },
-							{ "/", op_div },
-								{ "%", op_mod },
-									{ NULL, NULL }
-					};
-			int i = 0;
+	op_t ops[] = {
+		{ "+", op_add },
 
-				while (i < 5)
-						{
-									if (strcmp(s, ops[i].op) == 0)
-													return (ops[i].f);
+		{ "-", op_sub },
 
-											i++;
-												}
+		{ "*", op_mul },
 
-					return (0);
+		{ "/", op_div },
+
+		{ "%", op_mod },
+
+		{ NULL, NULL },
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+
+			return (ops[i].f);
+	i++;
+	}
+	return (0);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,30 +3,31 @@
 #include <stdio.h>
 
 /**
- *   * main - ...
- *     * @argc: ...
- *       * @argv: ...
- *         *
- *           * Return: ...
- *             */
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ *
+ * Return: ...
+*/
 int main(int argc, char *argv[])
 {
-		int (*oprt)(int, int);
+	int (*oprt)(int, int);
 
-			if (argc != 4)
-					{
-								printf("Error\n");
-										exit(98);
-											}
+	if (argc != 4)
+	{
+		printf("Error\n");
 
-				oprt = get_op_func(argv[2]);
+		exit(98);
+	}
 
-					if (!oprt)
-							{
-										printf("Error\n");
-												exit(99);
-													}
+		oprt = get_op_func(argv[2]);
 
-						printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-							return (0);
+		if (!oprt)
+		{
+			printf("Error\n")
+				exit(99);
+		}
+
+		printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+		return (0);
 }
