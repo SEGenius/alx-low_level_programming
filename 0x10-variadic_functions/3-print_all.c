e125a9560600ce9a454809cffd83b592348bd4d1
SEGenius <olutundestephen96@gmail.com>
2023-09-15 13:27:03 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -35,7 +35,7 @@ void print_all(const char * const format, ...)
 				str = va_arg(a_list, char*);
 				if (str == NULL)
 				str = "(nil)";
-				printf("%d", *str);
+				printf("%s", *str);
 				flag = 0;
 				break;
 				default:
