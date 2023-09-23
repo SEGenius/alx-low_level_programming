973c354c3a952bd66868ec57a8f40581418d1054
SEGenius <olutundestephen96@gmail.com>
2023-09-23 04:58:29 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	long int n, m, c, e, l;
+
+	n = 1;
+	m = 2;
+	e = 2;
+	c = 0;
+	l = 4000000;
+
+	while (c <= l)
+	{
+		c = m + n;
+		n = m;
+		m = c;
+
+		if ((c % 2 == 0) && (c <= l))
+		{
+			e = e + c;
+		}
+	}
+	printf("%li\n", e);
+
+	return (0);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,27 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * void: No return
  *
- * Return: Always 0.
+ * Return: success 1.
+ * On error, -1 is returned, and errno is set appropriately.
 */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int i, j;
 
-	for (a = 0; a <= 2; a++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (j = 0; j <= 59; j++)
 		{
-		if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-		{
-		for (c = 0; c <= 5; c++)
-		{
-		for (d = 0; d <= 9; d++)
-		{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(58);
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-					}
-					}
-					}
-					}
-					}
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(58);
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+
 }
